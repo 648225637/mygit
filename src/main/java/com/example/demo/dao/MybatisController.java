5992ed646b67ebb8b084b538338a6a2467c4004c
coco <648225637@qq.com>
2024-03-24 21:26:47 +0800

    Changes

--- src/main/java/com/example/demo/dao/MybatisController.java
+++ src/main/java/com/example/demo/dao/MybatisController.java
@@ -1,6 +1,11 @@
 package com.example.demo.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MybatisController {
+    @Autowired("/getJsonTest")
     public String getJsonTest(){
         return "1234567890";
     }
